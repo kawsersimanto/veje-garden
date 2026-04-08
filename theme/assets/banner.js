4590882b9c69e3299b9c1d25c7be5c520316910f
Kawser Simanto <kawsersimanto@gmail.com>
2026-04-08 15:52:58 +0600

    style(banner): made the banner section responsive. feat(banner): added shopify theme editor features

--- theme/assets/banner.js
+++ theme/assets/banner.js
@@ -1,7 +1,30 @@
-const bannerSlider = new Swiper(".banner-collection", {
-  slidesPerView: 1,
-  effect: "fade",
-  fadeEffect: {
-    crossFade: true,
-  },
+let bannerSlider = createBannerSlider();
+
+function createBannerSlider(startIndex = 0) {
+  const slider = new Swiper(".banner-collection", {
+    init: false,
+    initialSlide: startIndex,
+    slidesPerView: 1,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+  });
+  slider.init();
+  return slider;
+}
+
+document.addEventListener("shopify:block:select", (event) => {
+  const index = parseInt(event.target.dataset.index);
+  bannerSlider.slideTo(index);
+});
+
+document.addEventListener("shopify:section:load", () => {
+  const currentIndex = bannerSlider.activeIndex;
+  bannerSlider.destroy(true, true);
+  bannerSlider = createBannerSlider(currentIndex);
+});
+
+document.addEventListener("shopify:section:unload", () => {
+  bannerSlider.destroy(true, true);
 });
